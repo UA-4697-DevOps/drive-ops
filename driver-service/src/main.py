87b1fb26fc0aa98718a99002f070c77e3b181b27
KorotiukovaT <165308580+KorotiukovaT@users.noreply.github.com>
2026-01-07 13:46:48 +0200

    DriverService: add register driver endpoint

--- driver-service/src/main.py
+++ driver-service/src/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from uuid import uuid4
 
 app = FastAPI(title="DriverService", version="0.1.0")
+
+drivers = {}
 
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.post("/drivers", status_code=201)
+def register_driver():
+    driver_id = str(uuid4())
+
+    driver = {
+        "id": driver_id,
+        "status": "OFFLINE",
+        "location": None
+    }
+
+    drivers[driver_id] = driver
+    return driver
