dbe83e54e7fe6e9235cb2c0b7b77b77fc8ac4891
Davlit-ops <mozilla.davlit@gmail.com>
2026-02-09 11:38:45 +0000

    feat(vpc): add flow logs resources and iam roles with configurable retention

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -107,3 +107,65 @@ resource "aws_default_security_group" "default" {
     Name = "${var.project_name}-${var.env}-default-sg"
   }
 }
+
+# --- VPC Flow Logs (Cost-Optimized) ---
+
+resource "aws_cloudwatch_log_group" "flow_log" {
+  count             = var.enable_flow_logs ? 1 : 0
+  name              = "/aws/vpc-flow-log/${var.project_name}-${var.env}"
+  retention_in_days = var.flow_log_retention_in_days
+  tags              = { Name = "${var.project_name}-${var.env}-flow-log-group" }
+}
+
+resource "aws_iam_role" "flow_log_role" {
+  count = var.enable_flow_logs ? 1 : 0
+  name  = "${var.project_name}-${var.env}-vpc-flow-log-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = { Name = "${var.project_name}-${var.env}-vpc-flow-log-role" }
+}
+
+resource "aws_iam_role_policy" "flow_log_policy" {
+  count = var.enable_flow_logs ? 1 : 0
+  name  = "${var.project_name}-${var.env}-vpc-flow-log-policy"
+  role  = aws_iam_role.flow_log_role[0].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_flow_log" "main" {
+  count           = var.enable_flow_logs ? 1 : 0
+  iam_role_arn    = aws_iam_role.flow_log_role[0].arn
+  log_destination = aws_cloudwatch_log_group.flow_log[0].arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+  
+  tags = { Name = "${var.project_name}-${var.env}-flow-log" }
+}
