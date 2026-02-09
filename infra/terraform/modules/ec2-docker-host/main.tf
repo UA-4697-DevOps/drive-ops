df61e750a1fa3e910c0ed29ea69147797c6e7064
LeoM0303 <leomuliarchuk51@gmail.com>
2026-02-09 19:52:24 +0200

    feat:

--- infra/terraform/modules/ec2-docker-host/main.tf
+++ infra/terraform/modules/ec2-docker-host/main.tf
@@ -76,23 +76,18 @@ resource "aws_instance" "docker_host" {
   vpc_security_group_ids = [var.security_group_id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   key_name               = var.key_name
-
-
-metadata_options {
-http_endpoint               = "enabled"
-http_tokens                 = "required"
-http_put_response_hop_limit = 1
-}
-
-root_block_device {
-  encrypted = true
-} 
-
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+  root_block_device {
+    encrypted = true
+  }
   user_data = templatefile("${path.module}/user-data.sh", {
     service_name = var.service_name
     aws_region   = var.aws_region
   })
-
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.env}-${var.service_name}-host"
   })
