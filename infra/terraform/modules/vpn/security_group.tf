77a2b5d5f465c4d6b98ce97f1d63f512f86155e0
SophiaKrestova <ne_nashe_moloko@gmail.com>
2026-03-05 20:18:57 +0200

    add for_each

--- infra/terraform/modules/vpn/security_group.tf
+++ infra/terraform/modules/vpn/security_group.tf
@@ -16,26 +16,37 @@ resource "aws_security_group" "vpn" {
   })
 }
 
-# --- Ingress: OpenVPN (UDP 1194) from allowlisted CIDRs ---
+# --- Default security group rules ---
 
-resource "aws_security_group_rule" "vpn_ingress" {
-  type              = "ingress"
-  from_port         = 1194
-  to_port           = 1194
-  protocol          = "udp"
-  cidr_blocks       = var.allowed_vpn_cidrs
-  security_group_id = aws_security_group.vpn.id
-  description       = "OpenVPN client connections from allowlisted CIDRs"
+locals {
+  vpn_rules = {
+    ingress_vpn = {
+      type        = "ingress"
+      from_port   = 1194
+      to_port     = 1194
+      protocol    = "udp"
+      cidr_blocks = var.allowed_vpn_cidrs
+      description = "OpenVPN client connections from allowlisted CIDRs"
+    }
+    egress_all = {
+      type        = "egress"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow all outbound traffic (VPN routing + system updates)"
+    }
+  }
 }
 
-# --- Egress: allow all outbound (route VPN client traffic to VPC + internet) ---
+resource "aws_security_group_rule" "vpn_rules" {
+  for_each = local.vpn_rules
 
-resource "aws_security_group_rule" "vpn_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type              = each.value.type
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.protocol
+  cidr_blocks       = each.value.cidr_blocks
   security_group_id = aws_security_group.vpn.id
-  description       = "Allow all outbound traffic (VPN routing + system updates)"
+  description       = each.value.description
 }
